e9fe23b0c3c04718b1f9323360546749a920e96e
Douglas Ernandes <DouglasErnandes87@gmail.com>
2025-11-16 01:13:37 +0000

    Remova esses efeitos de waves/ondulações na hero section

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -60,11 +60,6 @@ const HeroSection = () => {
           <Badge variant="outline" className="border-green-400 text-green-300 bg-green-900/50 animate-float shadow-lg" style={{animationDelay: '0.4s'}}> <ShieldCheck className="h-4 w-4 mr-2 text-green-400"/> Equipe Qualificada</Badge>
         </div>
       </div>
-      <div className="wave-divider">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
-        </svg>
-      </div>
     </section>
   );
 };
